0159d812854a90da0c1045c85423c248da568f6f
denisaiacob <iacobdenisaalexandra49@gmail.com>
2022-10-22 11:24:20 +0300

    tema 3

--- Tema 3/Assets/Scripts/GrabScripts.cs
+++ Tema 3/Assets/Scripts/GrabScripts.cs
@@ -5,27 +5,15 @@ using UnityEngine;
 public class GrabScripts : MonoBehaviour
 {
     private Animator mAnimator;
-    bool leftHand=false;
-    bool rightHand=false;
     // Start is called before the first frame update
     void Start()
     {
         mAnimator=GetComponent<Animator>();
-        mAnimator.SetBool("RightHand",false);
-        mAnimator.SetBool("LeftHand",false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        while(Input.GetKeyDown("space"))
-        {
-            mAnimator.SetBool("RightHand",true);
-        }
-        if(Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            mAnimator.SetBool("LeftHand",true);
-        }
         if(Input.GetKeyDown(KeyCode.G))
         {
             mAnimator.SetTrigger("Grab");
